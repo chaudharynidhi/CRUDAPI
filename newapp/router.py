367b7433d9b05937d37ebe978bf397af9924b7c6
Nidhi Chaudhary <mynetworknidhi02@gmail.com>
2020-10-19 00:01:12 +0530

    Update router.py

--- newapp/router.py
+++ newapp/router.py
@@ -1,5 +1,5 @@
 from newapiapp import viewsets
 from rest_framework import routers
-
+# Rest Framework adds support for automatic URL routing to django and provides with simple way of writing the viewsets.
 router = routers.DefaultRouter()
 router.register('customer', viewsets.CustomerViewSets)
