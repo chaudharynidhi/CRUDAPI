61c74cf28d13e5b46e5e9e8bfe145da7a4451f62
Nidhi Chaudhary <mynetworknidhi02@gmail.com>
2020-10-18 23:56:39 +0530

    Update viewsets.py

--- newapiapp/viewsets.py
+++ newapiapp/viewsets.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets
 from .models import EcommerceCustomerData
 from .serializer import CustomerSerializer
 
+""" for creating the CRUD API view using ModelViewSet, used this as the implementation is easier for simple CRUD api as compared to AppViewSet """
+
 class CustomerViewSets(viewsets.ModelViewSet):
     queryset = EcommerceCustomerData.objects.all()
     serializer_class = CustomerSerializer
