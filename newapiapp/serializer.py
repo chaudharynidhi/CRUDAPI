3b6d4dded5c8ad4863563a6a939b5d7ce7601fa0
Nidhi Chaudhary <mynetworknidhi02@gmail.com>
2020-10-18 23:58:43 +0530

    Update serializer.py

--- newapiapp/serializer.py
+++ newapiapp/serializer.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import EcommerceCustomerData
 
+#used serializers to convert the complex model instances to native Python datatypes and those can be easily rendered into JSON
+
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = EcommerceCustomerData
